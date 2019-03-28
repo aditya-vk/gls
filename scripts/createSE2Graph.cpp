ce2ae2980ea9025ba4b30461b2cdc4226538723c
Aditya Vamsikrishna Mandalika <adityavk711@gmail.com>
2019-03-28 13:51:36 -0700

    add generation scripts

---
+++ scripts/createSE2Graph.cpp
@@ -0,0 +1,356 @@
+// Standard C++ libraries
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <queue>
+#include <Eigen/Dense>
+
+// Boost libraries
+#include <boost/shared_ptr.hpp>
+#include <boost/property_map/dynamic_property_map.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/graphml.hpp>
+#include <boost/function.hpp>
+#include <boost/program_options.hpp>
+
+// PRL libraries
+#include <aikido/io/CatkinResourceRetriever.hpp>
+#include <aikido/rviz/WorldInteractiveMarkerViewer.hpp>
+
+#include <dart/dart.hpp>
+#include <dart/utils/urdf/DartLoader.hpp>
+#include <dart/collision/CollisionGroup.hpp>
+#include <dart/collision/CollisionOption.hpp>
+#include <dart/collision/CollisionDetector.hpp>
+
+namespace po = boost::program_options;
+
+using dart::collision::CollisionDetectorPtr;
+using dart::collision::CollisionGroup;
+using dart::dynamics::SkeletonPtr;
+using aikido::rviz::WorldInteractiveMarkerViewer;
+
+static const std::string topicName("dart_markers");
+static const std::string baseFrameName("map");
+
+// =====================================================================================
+/// Displays a message to the user and waits for input.
+/// \param[in] message Message to show the user.
+void waitForUser(std::string message)
+{
+  std::string completeMessage = message + " Press [Enter] to continue.";
+  std::cout << completeMessage << std::endl;
+  std::cin.get();
+}
+
+// =====================================================================================
+/// Collision checks the robot and the world.
+/// \param[in] world The robot's environment.
+/// \param[in] robot The robot skeleton.
+/// \param[in] haltonPoint The configuration of the robot to be checked for collision.
+bool isPointValid(
+  SkeletonPtr world,
+  SkeletonPtr robot,
+  const Eigen::VectorXd haltonPoint
+  )
+{
+  // Convert positions to Eigen. DART To OMPL settings: angles, positions: ax, az, ay, x, z, y.
+  Eigen::VectorXd positions(6);
+  positions << 0.0, haltonPoint(2), 0.0, haltonPoint(0), 0.0, haltonPoint(1);
+
+  // Set Positions for the robot.
+  robot->setPositions(positions);
+
+  // Set up collision constraints for planning.
+  // TODO (avk): check if these are required.
+  CollisionDetectorPtr collisionDetector
+      = dart::collision::FCLCollisionDetector::create();
+  ::dart::collision::CollisionOption collisionOptions
+      = ::dart::collision::CollisionOption(
+          false,
+          1,
+          std::make_shared<::dart::collision::BodyNodeCollisionFilter>());
+  ::dart::collision::CollisionResult collisionResult;
+
+  std::shared_ptr<CollisionGroup> worldGroup
+      = collisionDetector->createCollisionGroup(world.get());
+
+  std::shared_ptr<CollisionGroup> robotGroup
+    = collisionDetector->createCollisionGroup(robot.get());
+
+  // Check collisions and return the result.
+  bool collisionStatus = collisionDetector->collide(worldGroup.get(), robotGroup.get(), collisionOptions,
+        &collisionResult);
+
+  return !collisionStatus;
+}
+
+// =====================================================================================
+/// Creates a DART skeleton from URDF.
+/// \param[in] resourceRetreiver Retrieves the URDF file.
+/// \param[in] uri Location of the URDF.
+/// \param[in] transform The pose of the skeleton.
+const SkeletonPtr makeBodyFromURDF(
+    const std::shared_ptr<aikido::io::CatkinResourceRetriever>
+        resourceRetriever,
+    const std::string& uri,
+    const Eigen::Isometry3d& transform)
+{
+  dart::utils::DartLoader urdfLoader;
+  const SkeletonPtr skeleton = urdfLoader.parseSkeleton(uri, resourceRetriever);
+
+  if (!skeleton)
+    throw std::runtime_error("unable to load '" + uri + "'");
+
+  dynamic_cast<dart::dynamics::FreeJoint*>(skeleton->getJoint(0))
+      ->setTransform(transform);
+  return skeleton;
+}
+
+// =====================================================================================
+/// Generates a Halton Point given the index
+/// \param[in] index Index in the sequence.
+/// \param[in] offset The offset to be applied to all the points in the sequence.
+/// \param[in] lowerLimits Lower positional limits of the robot joints.
+/// \param[in] upperLimits Upper positional limits of the robot joints.
+Eigen::VectorXd haltonSequence(
+    int index,
+    Eigen::VectorXd offset,
+    Eigen::VectorXd lowerLimits,
+    Eigen::VectorXd upperLimits)
+{
+  Eigen::VectorXd difference(3);
+  difference = upperLimits - lowerLimits;
+
+  // Catering to only ADA and HERB for now.
+  Eigen::VectorXd bases(3);
+  bases << 2, 3, 5;
+
+  // Generate the halton config
+  Eigen::VectorXd config(3);
+  for (int i = 0; i < 3; ++i)
+  {
+    int base = bases[i];
+    int tempIndex = index;
+    double result = 0;
+    double f = 1;
+
+    while (tempIndex > 0)
+    {
+      f = f/base;
+      result = result + f*(tempIndex % base);
+      tempIndex = tempIndex/base;
+    }
+    config[i] = result;
+  }
+
+  // Offset the halton sequence
+  config = config + offset;
+
+  // Wrap Around
+  for (int i = 0; i < 3; ++i)
+  {
+    if (config[i] > 1.0)
+      config[i] = config[i] - 1.0;
+    if (config[i] < 0.0)
+      config[i] = 1.0 + config[i];
+  }
+
+  // Scale the configurations
+  Eigen::VectorXd scaledConfig(3);
+  for (int i = 0; i < 3; ++i)
+  {
+    scaledConfig(i) = lowerLimits(i) + config(i) * difference(i);
+  }
+  return scaledConfig;
+}
+
+// =====================================================================================
+void generateHaltonPoints(SkeletonPtr world, SkeletonPtr robot, std::size_t numSamples, 
+                Eigen::VectorXd lowerLimits, Eigen::VectorXd upperLimits, double threshold)
+{
+
+  // Holds the vertices. Index is the line number. Content is the configuration.
+  std::string vertexFile = "/home/adityavk/workspaces/lab-ws/src/planning_dataset/vertices.txt";
+
+  // Holds the edge information. <source vertex ID> <target vertex ID> <length>
+  std::string edgesFile = "/home/adityavk/workspaces/lab-ws/src/planning_dataset/edges.txt";
+
+  // Holds each edge's source and target configurations. Useful to visualize the edges.
+  // Not necessary for graph generation.
+  std::string edgesVizFile = "/home/adityavk/workspaces/lab-ws/src/planning_dataset/edges_viz.txt";
+
+  // Generate configurations.
+  std::size_t numVertices = 0;
+  int index = 1;
+  std::srand((unsigned int) time(0));
+
+  // Generate a uniform offset
+  Eigen::VectorXd offset = Eigen::VectorXd::Random(3);
+
+  std::vector<Eigen::VectorXd> configurations;
+  while (true)
+  {
+    if (numVertices % 100 == 0)
+      std::cout << numVertices << std::endl;
+
+    Eigen::VectorXd config = haltonSequence(index, offset, lowerLimits, upperLimits);
+
+    index = index + 1;
+
+    if(!isPointValid(world, robot, config))
+      continue;
+    else
+      configurations.emplace_back(config);
+
+    if (numVertices == numSamples)
+      break;
+  }
+
+  // Insert all the vertices into the vertexFile.
+  std::ofstream vertexLogFile;
+  vertexLogFile.open(vertexFile, std::ios_base::app);
+
+  for(const auto& config : configurations)
+    vertexLogFile << config[0] << " " << config[1] << " " << config[2] << std::endl;
+
+  vertexLogFile.close();
+
+  // =================================================
+  // Vertices generated. Now we try to make the edges.
+  // =================================================
+  std::ofstream edgesLogFile;
+  edgesLogFile.open(edgesFile, std::ios_base::app);
+
+  std::ofstream edgesVizLogFile;
+  edgesVizLogFile.open(edgesVizFile, std::ios_base::app);
+
+  assert(configurations.size() == numSamples);
+
+  std::size_t connectedEdges = 0;
+
+  for (std::size_t i = 0; i < configurations.size()-1; ++i)
+  {
+    if (i % 100 == 0)
+      std::cout << "Currently on " << i << std::endl;
+    for (std::size_t j = i+1; j < configurations.size(); ++j)
+    {
+      // Check if vertices are closeby.
+      auto distance = (configurations[i] - configurations[j]).norm();
+
+      if (distance > threshold)
+        continue;
+
+      connectedEdges++;
+
+      edgesLogFile << i << " " << j << " " << distance << std::endl;
+
+      for (int index = 0; index < 2; ++index)
+      {
+        edgesVizLogFile << configurations[i](index) << " ";
+      }
+
+      for (int index = 0; index < 2; ++index)
+      {
+        edgesVizLogFile << configurations[j](index) << " ";
+      }
+      edgesVizLogFile << std::endl;
+
+    }
+  }
+  edgesLogFile.close();
+  std::cout << "Total Vertices " << configurations.size() << " Edge " << connectedEdges << std::endl;
+}
+
+// =====================================================================================
+int main(int argc, char *argv[])
+{
+  po::options_description desc("HERB Planner Options");
+  desc.add_options()
+      ("help,h", "produce help message")
+      ("world,w", po::value<std::string>()->required(), "world to load")
+      ("robot,r", po::value<std::string>()->required(), "robot to load")
+      ("number,n", po::value<std::size_t>()->required(), "number of vertices in the graph")
+      ("lower,l", po::value<std::vector<double>>()->required(), "lower limits")
+      ("upper,u", po::value<std::vector<double>>()->required(), "upper limits")
+  ;
+
+  // Read arguments
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("help"))
+  {
+    std::cout << desc << std::endl;
+    return 1;
+  }
+
+  std::string worldName(vm["world"].as<std::string>());
+  std::string robotName(vm["robot"].as<std::string>());
+  std::size_t numSamples(vm["number"].as<std::size_t>());
+  std::vector<double> lLimits(vm["lower"].as<std::vector<double>>());
+  std::vector<double> uLimits(vm["upper"].as<std::vector<double>>());
+
+  /// HERB ENVIRONMENT
+  ROS_INFO("Starting ROS node.");
+  ros::init(argc, argv, "ompl_app");
+  ros::NodeHandle nh("~");
+
+  // Create AIKIDO World
+  aikido::planner::WorldPtr env(new aikido::planner::World("ompl_env"));
+
+  // Visualization topics
+  static const std::string execTopicName = topicName + "/exec";
+
+  // Start the RViz viewer.
+  ROS_INFO_STREAM("Starting viewer. Please subscribe to the '" << execTopicName << "' InteractiveMarker topic in RViz.");
+  WorldInteractiveMarkerViewer viewer(env, execTopicName, baseFrameName);
+  viewer.setAutoUpdate(true);
+
+  // Resolves package:// URIs by emulating the behavior of 'catkin_find'.
+  const auto resourceRetriever = std::make_shared<aikido::io::CatkinResourceRetriever>();
+  const std::string worldURDFUri = "package://pr_assets/data/ompl/" + worldName + ".urdf";
+  const std::string robotURDFUri = "package://pr_assets/data/ompl/" + robotName + ".urdf";
+
+  // Initial perception
+  SkeletonPtr world;
+  Eigen::Isometry3d worldPose;
+
+  SkeletonPtr robot;
+  Eigen::Isometry3d robotPose;
+
+  // Poses for world
+  worldPose = Eigen::Isometry3d::Identity();
+
+  // Poses for robot
+  robotPose = Eigen::Isometry3d::Identity();
+
+  // Load objects
+  world = makeBodyFromURDF(resourceRetriever, worldURDFUri, worldPose);
+  robot = makeBodyFromURDF(resourceRetriever, robotURDFUri, robotPose);
+
+  // Add all objects to World
+  env->addSkeleton(world);
+  env->addSkeleton(robot);
+
+  // Set the right limits.
+  Eigen::VectorXd lowerLimits;
+  Eigen::VectorXd upperLimits;
+  lowerLimits << lLimits[0], lLimits[1], -3.14;
+  upperLimits << uLimits[0], uLimits[1], 3.14;
+
+  Eigen::VectorXd difference(2);
+  difference = upperLimits - lowerLimits;
+  double threshold = 5*std::pow(numSamples, -1.0/3)*std::max(difference(0), difference(1));
+
+  waitForUser("The environment has been setup. Press key to start generating the graph");
+
+  // Generate the Halton Points.
+  generateHaltonPoints(world, robot, numSamples, lowerLimits, upperLimits, threshold);
+
+  waitForUser("Press enter to exit");
+  return 0;
+}
---
+++ scripts/createSE2Graph.py
@@ -0,0 +1,48 @@
+from IPython import embed
+import argparse
+import numpy
+import networkx as nx
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='script for generating obstacle files')
+
+    args = parser.parse_args()
+
+    G = nx.Graph()
+
+    directory = "/home/adityavk/workspaces/lab-ws/src/planning_dataset/"
+    default_vertex_location = directory + 'vertices.txt'
+    default_edges_location = directory + 'edges.txt'
+    edgeStates_location = directory + 'edgesViz.txt'
+    graph_save_location = directory + 'graph_se2_apartment.graphml'
+
+    # Collect vertices.
+    halton = numpy.loadtxt(default_vertex_location)
+    n = numpy.shape(halton)[0]
+
+    # Add vertices
+    print("Adding vertices")
+    for i in range(n):
+        state = ''
+        for j in range(6):
+            state += str(halton[i][j]) + ' '
+        state += str(halton[i][-1])
+
+        G.add_node(i, state = state)
+
+    # Collect edges.
+    haltonEdges = numpy.loadtxt(default_edges_location)
+    edgeStates = numpy.loadtxt(edgeStates_location)
+
+    # Adding Edges
+    print("Adding edges")
+    for i in range(numpy.shape(haltonEdges)[0]):
+        dist = haltonEdges[i][-1]
+        G.add_edge(int(haltonEdges[i][0]), int(haltonEdges[i][1]), length = str(dist))
+
+    print('Average degree: ', G.number_of_edges() * 2.0 / n)
+    print('Connected: ', nx.is_connected(G))
+    print('Number of Connected Components: ', nx.number_connected_components(G))
+    nx.write_graphml(G, graph_save_location)
+    
